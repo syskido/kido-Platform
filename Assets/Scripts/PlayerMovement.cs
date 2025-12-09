f219c9fd473c30131b00eeb000b73a1516327fa1
yunkido <syskido82@gmail.com>
2025-12-09 21:43:28 +0900

    run & grounded & jump 업로드

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,25 +6,47 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
     [SerializeField] private float speed;
+    private Animator anim;
+    private bool grounded;
+
 
     private void Awake()
     {
+        //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        float horzInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horzontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horzontalInput * speed, body.velocity.y);
 
         //Flip player when moving left/right
-        if (horzInput > 0.01f)
+        if (horzontalInput > 0.01f)
             transform.localScale = Vector3.one;
-
-        else if (horzInput < -0.01f)
+        else if (horzontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        //Set animator parameters
+        anim.SetBool("run", horzontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Check if player is grounded
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
