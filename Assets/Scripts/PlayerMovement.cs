96a832d7caa8deb4597cfd91f68620220c86bd0e
yunkido <syskido82@gmail.com>
2025-12-10 13:46:18 +0900

    isGround 업로드

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D body;
     [SerializeField] private float speed;
+    [SerializeField] private LayerMask groundLayer;
+    private Rigidbody2D body;    
     private Animator anim;
     private bool grounded;
-
+    private BoxCollider2D boxCollider;
 
     private void Awake()
     {
         //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -28,25 +28,28 @@ public class PlayerMovement : MonoBehaviour
         else if (horzontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        if (Input.GetKey(KeyCode.Space) && grounded)
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
             Jump();
 
         //Set animator parameters
         anim.SetBool("run", horzontalInput != 0);
-        anim.SetBool("grounded", grounded);
+        anim.SetBool("grounded", isGrounded());
     }
 
     private void Jump()
     {
         body.velocity = new Vector2(body.velocity.x, speed);
-        anim.SetTrigger("jump");
-        grounded = false;
+        anim.SetTrigger("jump");     
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Check if player is grounded
-        if (collision.gameObject.tag == "Ground")
-            grounded = true;
+    }
+ 
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
     }
 }
