bd387dfa432a39364eaf995ac280f86d5d605785
yunkido <syskido82@gmail.com>
2025-12-12 19:17:36 +0900

    Firetrap 잘 안되고 있음

---
+++ Assets/Firetrap.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+
+public class Firetrap : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    [Header ("Firetrap Times")]
+    [SerializeField] private float activationDelay;
+    [SerializeField] private float activeTime;
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+
+    private bool triggered; //when the trap gets triggered
+    private bool active; //when the trap is active and can hurt the player
+
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && !triggered)
+        {
+            if (!triggered)
+            {
+                StartCoroutine(ActivateFiretrap());
+            }
+            if (active)
+                collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+    private IEnumerator ActivateFiretrap()
+    {
+        triggered = true;
+        spriteRend.color = Color.red;
+        yield return new WaitForSeconds(activationDelay);
+        anim.SetBool("activated", true);
+
+        spriteRend.color = Color.white;
+        active = true;
+        yield return new WaitForSeconds(activeTime);
+        active = false;
+        triggered = false;
+        anim.SetBool("activated", false);
+    }
+}
