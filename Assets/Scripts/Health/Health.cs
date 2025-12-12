6e32298810c817f965c02f74c7a9d19b608e6a10
yunkido <syskido82@gmail.com>
2025-12-12 16:39:37 +0900

    Saw 업로드

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -4,10 +4,13 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
     }
 
     public void TakeDamage(float _damage)
@@ -16,11 +19,16 @@ public class Health : MonoBehaviour
 
         if (currentHealth > 0)
         {
-            // Player is still alive
+            anim.SetTrigger("hurt");
         }
         else
         {
-            // Player is dead
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }           
         }
     }
 
@@ -31,6 +39,4 @@ public class Health : MonoBehaviour
             TakeDamage(1);
         }
     }
-
-
 }
